82dec082d96f9f4b7d116e767ea7f7a221e60102
Saadkhalid913 <Saadkhalid913@gmail.com>
2021-09-27 21:37:20 -0400

    Added position event listener to canvas onmousemove

--- flow/src/components/Room.jsx
+++ flow/src/components/Room.jsx
@@ -1,9 +1,9 @@
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useContext, useState, useEffect, useRef } from 'react'
 import socketContext from '../contexts/socketContext'
 
 const Room = (props) => {
     const [text, setText] = useState("")
-
+    const canvas = useRef()
 
     const Socket = useContext(socketContext)
     useEffect(() => {
@@ -14,13 +14,17 @@ const Room = (props) => {
         return () => Socket.off("text-changed", handler)
     }, [setText, Socket])
 
+    if (canvas.current) {
+        canvas.current.addEventListener("mousemove", e => console.log(e.layerX, e.layerY))
+    }
 
     return (
-    <div>
-        <textarea value = {text} onChange = {(e) => {
+    <div className = "room-page-wrapper">
+        {/* <textarea value = {text} onChange = {(e) => {
             setText(e.target.value)
             Socket.emit("update-text", e.target.value, props.match.params.id)
-            }} /> 
+            }} />  */}
+            <canvas ref = {canvas}  width = {900} height = {600}/>
     </div>
     )
 }
