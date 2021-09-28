92276fab5d50eaa57060b12c9c122ef786d28f3c
Saadkhalid913 <Saadkhalid913@gmail.com>
2021-09-27 22:02:30 -0400

    got drawing function to work on canvas

--- flow/src/components/Canvas.jsx
+++ flow/src/components/Canvas.jsx
@@ -1,17 +1,35 @@
-import React, {useRef} from 'react'
+import React, {useRef, useEffect} from 'react'
 
  const Canvas = (props) => {
     const canvas = useRef()
-    let MouseDown = false 
-    if (canvas.current) {
-        canvas.current.addEventListener("mousedown", () => MouseDown = true)
-        canvas.current.addEventListener("mouseup", () => MouseDown = false)
+    let MouseDown = useRef(false)
+    
+    useEffect(() => {
 
-        canvas.current.addEventListener("mousemove", e => {
-            if (MouseDown) console.log(e.layerX, e.layerY)
-        })
+        
+        if (canvas.current) {
+            const ctx = canvas.current.getContext("2d")
+            const draw = (x, y) => {
+                ctx.lineWidth = 10;
+                ctx.lineCap = "round";
 
-    }
+                ctx.lineTo(x,y);
+                ctx.stroke();
+                ctx.beginPath();
+                ctx.moveTo(x,y);
+            }
+
+            canvas.current.addEventListener("mousedown", () => MouseDown.current = true)
+            canvas.current.addEventListener("mouseup", () => MouseDown.current = false)
+            canvas.current.addEventListener("mousemove", e => {
+                if (MouseDown.current) {
+                    console.log(e.layerX, e.layerY)
+                    draw(e.layerX, e.layerY)
+                }
+            })
+    
+        }
+    })
 
     return <canvas ref = {canvas} width = {props.width} height = {props.height} />
 }
